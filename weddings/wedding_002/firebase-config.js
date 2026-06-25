5892bbfe23184bc6b6a3a4f48f0db3d7a5bc70b9
friendlyedupreneur <friendlyedupreneur@gmail.com>
2026-06-25 14:50:56 +0700

    Update firebase-config.js

--- weddings/wedding_002/firebase-config.js
+++ weddings/wedding_002/firebase-config.js
@@ -1,16 +1,1 @@
-/*
- * Firebase config - Dark Luxury Cinematic Wedding Template
- */
 
-window.WEDDING_FIREBASE_ENABLED = true;
-window.WEDDING_COLLECTION = "weddings";
-
-window.firebaseConfig = {
-  apiKey: "AIzaSyAfI1UUHUqnpImUpX_fsH_pTeJGZcUcG8s",
-  authDomain: "rezaintan-wedding.firebaseapp.com",
-  projectId: "rezaintan-wedding",
-  storageBucket: "rezaintan-wedding.firebasestorage.app",
-  messagingSenderId: "834037181305",
-  appId: "1:834037181305:web:4d8335917243a408759da8",
-  measurementId: "G-WDJEYXSV6M"
-};
